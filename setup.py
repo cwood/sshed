dfb631e4ecc95377ea6d550e119e570b57b72429
Colin G. Wood <cwood06@gmail.com>
2012-09-15 23:03:47 -0400

    Update setup.py to work with pypi.python.org

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from distutils.core import setup
+from sshed import version
 
 setup(
     name='sshed',
     author='Colin Wood',
     author_email='cwood06@gmail.com',
     install_requires=[
-        'paramiko',
-    ]
+        'paramiko>=1.7.7.2',
+    ],
+    long_description=open('README.markdown').read(),
+    url='http://cwood.github.com/sshed/',
+    version=version,
+    packages=['sshed'],
+    description='Minimal Parmaiko wrapper to make working with SSH easy.',
 )
