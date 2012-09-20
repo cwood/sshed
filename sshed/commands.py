4538eba7189dd257716f5093b83abda2b10e45e4
Colin Wood <cwood06@gmail.com>
2012-09-19 23:22:16 -0400

    Docstring for command

--- sshed/commands.py
+++ sshed/commands.py
@@ -1,5 +1,12 @@
 class Command(object):
 
+    """
+        sshed server.run returns back a command object. This object should
+        have the return code, stderr, stdout and a couple functions.
+
+        Current this allows a script to retry a command.
+    """
+
     def __init__(cmd, command_str, host):
         cmd.command_str = command_str
         cmd.host = host
